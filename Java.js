7aa89424575d153af91676530d4ead067eda3ce0
Yamada-Hibiki <100133616+Yamada-Hibiki@users.noreply.github.com>
2022-03-21 21:53:51 +0100

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,11 @@
+function showDiv(id) {
+    var div = document.getElementById(id);
+    if (div.style.display == "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  
+
+  
